5ebc2e6f562f21551446936deaa750998ce2fa10
dorra-kallel <dorra.kallel@esprit.tn>
2023-07-07 12:17:55 +0200

    main

---
+++ Contenaire.Infrastrcture/AMContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using Contenaire.Core.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contenaire.Infrastrcture
+{
+   
+    public class AMContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\mssqllocaldb;
+              Initial Catalog=AirportManagementDB;Integrated Security=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+
+        public AMContext()
+        {
+
+        }
+
+        public AMContext(DbContextOptions<AMContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+
+        }
+
+
+
+    }
+}
---
+++ Contenaire.Web/Program.cs
@@ -0,0 +1,58 @@
+using Contenaire.Core.Interfaces;
+using Contenaire.Infrastrcture;
+using Contenaire.Core.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using System.Reflection;
+using System.IO;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<DbContext, AMContext>(options =>
+    options.UseLazyLoadingProxies().UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "JwtApp", Version = "v1" });
+
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
+    }
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "JwtApp v1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+//Working with Static Files
+app.UseStaticFiles();
+// Matches request to an endpoint.
+app.UseRouting();
+
+// Use the default authorization.
+app.UseAuthorization();
+
+app.UseAuthentication();
+
+app.MapControllers();
+
+app.Run();
